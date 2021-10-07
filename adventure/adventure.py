1ad4cd8e0225016a913e26d761a8457d253e8a2f
aikaterna <20862007+aikaterna@users.noreply.github.com>
2021-10-07 09:26:53 -0700

    Version number change

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -93,7 +93,7 @@ class Adventure(
             user_id
         ).clear()  # This will only ever touch the separate currency, leaving bot economy to be handled by core.
 
-    __version__ = "3.4.3.2"
+    __version__ = "3.5.0"
 
     def __init__(self, bot: Red):
         self.bot = bot
